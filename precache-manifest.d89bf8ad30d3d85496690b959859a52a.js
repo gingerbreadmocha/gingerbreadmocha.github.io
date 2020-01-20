a949494079f410caa78ce4b1712bed316201dd34
gingerbreadmocha <jennyzhou0302@gmail.com>
2020-01-19 18:05:24 -0800

    Portfolio update

---
+++ precache-manifest.d89bf8ad30d3d85496690b959859a52a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633ff4bb4a3d9ac9702cdba2440feedc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a013c2131d306437089",
+    "url": "/static/css/2.28dcdecb.chunk.css"
+  },
+  {
+    "revision": "3ae0cfba8e3161fc65c1",
+    "url": "/static/css/main.bfe0df53.chunk.css"
+  },
+  {
+    "revision": "5a013c2131d306437089",
+    "url": "/static/js/2.ee4307d9.chunk.js"
+  },
+  {
+    "revision": "823e2026238c53cb38aa5921fcf10ad7",
+    "url": "/static/js/2.ee4307d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ae0cfba8e3161fc65c1",
+    "url": "/static/js/main.76b60cc9.chunk.js"
+  },
+  {
+    "revision": "bc2cf3dc1f69b0765c39",
+    "url": "/static/js/runtime-main.086f053c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
